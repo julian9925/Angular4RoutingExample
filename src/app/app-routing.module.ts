72decf406770a83b81e0e79b343749603d01421c
Julian Chen <julius.j@inwinstack.com>
2017-06-11 10:55:37 +0800

    Seperate route path into individual route.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { Page2Component } from './page2/page2.component';
 import { Page1Component } from './page1/page1.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { fallbackRoute } from "./fallback-route";
 
 const routes: Routes = [
   { path: '', redirectTo: '/page1', pathMatch: 'full'},
   // { path: '', component: Page1Component},
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
-  { path: '**', redirectTo: '/page1', pathMatch: 'full' },
+  fallbackRoute
 ];
 
 @NgModule({
