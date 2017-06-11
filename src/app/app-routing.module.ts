ad1e8fbd9c94bf92d8549a8e3cfbe7be1eb14876
Julian Chen <julius.j@inwinstack.com>
2017-06-11 10:48:11 +0800

    Demo all path redirected to certain path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/page1', pathMatch: 'full'},
+  // { path: '', component: Page1Component},
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
+  { path: '**', redirectTo: '/page1', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
