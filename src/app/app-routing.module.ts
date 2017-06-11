e3d76c81f282267f3a647bc765b74634d3305367
Julian Chen <julius.j@inwinstack.com>
2017-06-11 16:44:55 +0800

    add login component and make app.component.html clean

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { fallbackRoute } from "./fallback-route";
@@ -5,11 +7,16 @@ import { DashboradComponent } from './dashborad/dashborad.component';
 import { CardComponent } from './card/card.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  { path: 'dashboard', component: DashboradComponent },
-  { path: 'cards/:type', component: CardComponent },
-  { path: 'charts', loadChildren: './charts/charts.module#ChartsModule'}
-  // fallbackRoute
+  { path: '', component: LayoutComponent,
+    children:[
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+      { path: 'dashboard', component: DashboradComponent },
+      { path: 'cards/:type', component: CardComponent },
+      { path: 'charts', loadChildren: './charts/charts.module#ChartsModule'}
+    ]
+  },
+  { path: 'login', component: LoginComponent},
+  fallbackRoute
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboradComponent } from './dashborad/dashborad.component';
 import { CardComponent } from './card/card.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboradComponent,
     CardComponent,
+    LayoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
