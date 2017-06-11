a01de3966eeb9a5e701213240d98d03c9bcc44e7
Julian Chen <julius.j@inwinstack.com>
2017-06-11 09:30:39 +0800

    testing routing module with routes config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
+import { Page2Component } from './page2/page2.component';
+import { Page1Component } from './page1/page1.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
 ];
 
 @NgModule({
